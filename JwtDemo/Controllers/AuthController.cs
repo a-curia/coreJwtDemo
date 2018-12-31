cf352abcf8a62b85bbcb8869e50bfe4e91e138d5
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2018-12-31 19:28:55 +0200

    first

---
+++ JwtDemo/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost("token")]
+        public ActionResult GetToken()
+        {
+            // security key
+            string securityKey = "this_is_our_supper_long_security_key_for_token_validation_project_f$3224.in";
+
+            // symetic security key
+            var symmeticSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            // signing credential
+            var signingCredentials = new SigningCredentials(symmeticSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            // add claims
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role,"Administrator"));
+            claims.Add(new Claim(ClaimTypes.Role, "Reader"));
+            claims.Add(new Claim("OurCustomClaim","CutomClaimValue"));
+
+            // create token
+            var token = new JwtSecurityToken(
+                    issuer:"anystring.com",
+                    audience: "any string readers",
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: signingCredentials,
+                    // assign claims to out token
+                    claims: claims
+                );
+
+            // return token
+            var strToken  = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(strToken);
+        }
+    }
+}
---
+++ JwtDemo/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string securityKey = "this_is_our_supper_long_security_key_for_token_validation_project_f$3224.in";
+            var symmeticSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => { options.TokenValidationParameters = 
+                    new TokenValidationParameters {
+                        // what to validate
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        // setup validate data
+                        ValidIssuer = "anystring.com",
+                        ValidAudience = "any string readers",
+                        IssuerSigningKey = symmeticSecurityKey
+                    };
+                });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
